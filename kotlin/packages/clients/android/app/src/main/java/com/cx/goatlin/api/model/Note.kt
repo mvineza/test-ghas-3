69450876b4471fe4786196563c51fda9bc6aee52
merrellvv <rell.vineza14@yahoo.com>
2023-02-23 09:19:49 +0800

    main: remove kotlion

--- kotlin/packages/clients/android/app/src/main/java/com/cx/goatlin/api/model/Note.kt
+++
@@ -1,3 +0,0 @@
-package com.cx.goatlin.api.model
-
-data class Note (var title: String, var content: String, var createAt: String)
--- kotlin/packages/clients/android/app/src/main/java/com/cx/goatlin/models/Note.kt
+++
@@ -1,6 +0,0 @@
-package com.cx.goatlin.models
-
-data class Note(var title: String, var content: String) {
-    var id: Int = -1
-    var owner: Int = -1
-}
